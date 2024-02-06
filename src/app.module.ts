529cae7f7883e427c979e9485432025b01fe463a
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-02-06 10:28:18 +0800

    feat: add webscoket

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { ConfigModule } from '@nestjs/config';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
   imports: [TodoModule, ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ChatGateway],
 })
 export class AppModule {}
---
+++ src/chat.gateway.ts
@@ -0,0 +1,46 @@
+import { Logger } from '@nestjs/common';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  private readonly logger = new Logger(ChatGateway.name);
+  @WebSocketServer() io: Server;
+
+  afterInit() {
+    this.logger.log('Initialized');
+  }
+
+  handleConnection(client: any, ...args: any[]) {
+    // Handle connection event
+    const { sockets } = this.io.sockets;
+
+    this.logger.log(`Client id: ${client.id} connected`);
+    this.logger.debug(`Number of connected clients: ${sockets.size}`);
+  }
+
+  handleDisconnect(client: any) {
+    this.logger.log(`Cliend id:${client.id} disconnected`);
+  }
+
+  @SubscribeMessage('ping')
+  handleMessage(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: string,
+  ) {
+    this.logger.log(`Message received from client id: ${client.id}`);
+    this.logger.debug(`Payload: ${data}`);
+    return {
+      event: 'pong',
+      data,
+    };
+  }
+}
